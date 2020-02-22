4959acce5125079966768fc7d1e9844601aace75
Aleksandra Kuznetsova <kuznesashka@gmail.com>
2020-02-22 14:58:57 +0300

    Add files via upload
    LCMV and MNE calculations

---
+++ mne.m
@@ -0,0 +1,18 @@
+function Zmne = mne(G2dU_red, Ca, lambda)
+        [Nch, Nsites_red] = size(G2dU_red);
+        Nsites_red = Nsites_red/2;
+        Cs = eye(Nsites_red*2);
+        Cn = eye(size(G2dU_red,1));
+        GGt = G2dU_red*Cs*G2dU_red';
+        Wmne = G2dU_red'*inv(G2dU_red*Cs*G2dU_red'+lambda*trace(GGt)/size(GGt,1)*Cn);
+
+        clear Zmne;
+        range2d = 1:2;
+        for i = 1:Nsites_red
+            w = Wmne(range2d,:);
+            m = w*Ca*w';
+            [u ss v] = svd(m);
+            Zmne(i) = ss(1,1);
+            range2d = range2d+2;
+        end
+end
