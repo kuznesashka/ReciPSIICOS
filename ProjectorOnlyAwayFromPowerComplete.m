1501b65fa424543ed84fb9f5c74707abc28df953
Aleksandra Kuznetsova <kuznesashka@gmail.com>
2022-11-11 02:33:28 +0300

    Update ProjectorOnlyAwayFromPowerComplete.m

--- ProjectorOnlyAwayFromPowerComplete.m
+++ ProjectorOnlyAwayFromPowerComplete.m
@@ -1,46 +1,45 @@
-function [Upwr,ds,A] = ProjectorOnlyAwayFromPowerComplete(G2dU, PwrRnk, fixed)
+function [Upwr, ds, A] = ProjectorOnlyAwayFromPowerComplete(G2dU, PwrRnk, fixed)
 
-if(nargin<2)
+if(nargin < 2)
     PwrRnk = 350;
 end
 
 if fixed == 0
-    Ns = size(G2dU,2)/2; % two topography columns per each source of the grid
+    Ns = size(G2dU, 2) / 2; % two topography columns per each source of the grid
     % perform projection of the coherence matrix away from the power only
-    Nch = size(G2dU,1);
+    Nch = size(G2dU, 1);
 
     % project for each potential source
     fprintf('Collecting power subspace of coherence span...\n');
-    A = zeros(Nch^2,Ns*3);
-    for i=1:Ns
-         gi = G2dU(:,2*i-1);
-         v = gi*gi';
-         A(:,3*i-2) = v(:)/norm(v(:));
-         gj = G2dU(:,2*i);
-         v = gj*gj';
-         A(:,3*i-1) = v(:)/norm(v(:));
-         v = gi*gj'+gj*gi';
-         A(:,3*i) = v(:)/norm(v(:));
+    A = zeros(Nch^2, Ns * 3);
+    for i = 1:Ns
+         gi = G2dU(:, 2 * i - 1);
+         v = gi * gi';
+         A(:, 3 * i - 2) = v(:) / norm(v(:));
+         gj = G2dU(:, 2 * i);
+         v = gj * gj';
+         A(:, 3 * i - 1) = v(:) / norm(v(:));
+         v = gi * gj' + gj * gi';
+         A(:, 3 * i) = v(:) / norm(v(:));
     end
 else
-    Ns = size(G2dU,2);
-    Nch = size(G2dU,1);
+    Ns = size(G2dU, 2);
+    Nch = size(G2dU, 1);
 
     % project for each potential source
     fprintf('Collecting power subspace of coherence span...\n');
-    A = zeros(Nch^2,Ns);
+    A = zeros(Nch ^ 2, Ns);
     for i = 1:Ns
-         gi = G2dU(:,i);
-         v = gi*gi';
-         A(:,i) = v(:)/norm(v(:));
+         gi = G2dU(:, i);
+         v = gi * gi';
+         A(:, i) = v(:) / norm(v(:));
     end
 end
 
 fprintf('Finding eigen space...\n');
-AA = A*A';
-[u s] = eigs(AA,PwrRnk);
+AA = A * A';
+[u s] = eigs(AA, PwrRnk);
 ds = diag(s);
-Upwr = u(:,1:PwrRnk);
+Upwr = u(:, 1:PwrRnk);
 
 end
-
