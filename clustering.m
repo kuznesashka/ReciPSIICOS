f6b9c9d3f78138b124d9038bbe54284da4cd5957
Aleksandra Kuznetsova <kuznesashka@gmail.com>
2022-11-11 02:35:26 +0300

    Update clustering.m

--- clustering.m
+++ clustering.m
@@ -1,11 +1,11 @@
 function [cluster, center, maxind, clustnum] = clustering(Z, R_red, thr_dist)
     
-    for frac = 1:size(Z,2)
-        src_idx = find(Z(:,frac) > 0)';
+    for frac = 1:size(Z, 2)
+        src_idx = find(Z(:, frac) > 0)';
 
         for i = 1:size(src_idx, 2) % distances between each vertex
             for j = 1:size(src_idx, 2)
-                dist(i,j) = norm(R_red(src_idx(i),:)-R_red(src_idx(j),:));
+                dist(i, j) = norm(R_red(src_idx(i), :) - R_red(src_idx(j), :));
             end
         end
 
@@ -16,18 +16,18 @@ function [cluster, center, maxind, clustnum] = clustering(Z, R_red, thr_dist)
             dst = sum(dist < thr_dist, 2); 
             [val, ind] = max(dst); % vertex with the highest number of close neighbours
             if val >= Nmin
-                ind_nbh = find(dist(ind,:) < thr_dist); % neighbours
-                cluster{frac, k} = src_idx(:,ind_nbh);
-                center{frac, k} = mean(R_red(cluster{frac, k},:));
+                ind_nbh = find(dist(ind, :) < thr_dist); % neighbours
+                cluster{frac, k} = src_idx(:, ind_nbh);
+                center{frac, k} = mean(R_red(cluster{frac, k}, :));
                 [val, indm] = max(Z(cluster{frac, k}));
                 maxind{frac, k} = cluster{frac, k}(indm);
-                src_idx = src_idx(:,setdiff(1:size(dist,1), ind_nbh));
-                dist = dist(setdiff(1:size(dist,1), ind_nbh),setdiff(1:size(dist,1), ind_nbh));
+                src_idx = src_idx(:, setdiff(1:size(dist, 1), ind_nbh));
+                dist = dist(setdiff(1:size(dist, 1), ind_nbh), setdiff(1:size(dist, 1), ind_nbh));
                 k = k + 1;
                 fl = 1;
             else
                 fl = 0;
-                clustnum(frac) = k-1;
+                clustnum(frac) = k - 1;
             end
         end
     end
