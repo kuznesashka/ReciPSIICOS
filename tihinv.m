2f3ca37889dfcc0774ef8258c7062794c6ad412c
Aleksandra Kuznetsova <kuznesashka@gmail.com>
2022-11-11 02:49:05 +0300

    Update tihinv.m

--- tihinv.m
+++ tihinv.m
@@ -1,11 +1,11 @@
 function iM = tihinv(M, alpha)
 
-if(nargin==1)
+if(nargin == 1)
     alpha = 0.00001;
 end;
 
 MeanDiag = mean(abs(diag(M)));
 
-MTikh = M+MeanDiag*alpha*eye(size(M,1));
+MTikh = M + MeanDiag * alpha * eye(size(M, 1));
 
 iM = inv(MTikh);
